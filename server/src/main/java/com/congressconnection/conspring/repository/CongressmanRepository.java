17f997f0d58097adf7e771c9a802c2f0047b2550
SagarPatel201 <sagarpatel201@gmail.com>
2022-03-12 19:14:12 -0500

    Implement Congressman Spring repository class

---
+++ server/src/main/java/com/congressconnection/conspring/repository/CongressmanRepository.java
@@ -0,0 +1,7 @@
+package com.congressconnection.conspring.repository;
+import com.congressconnection.conspring.model.Congressman;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CongressmanRepository extends JpaRepository<Congressman, String> {}
