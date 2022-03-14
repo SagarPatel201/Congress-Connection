f3d4dada364773c1fabb604f7bdeac8eb52dcb70
SagarPatel201 <sagarpatel201@gmail.com>
2022-03-14 14:36:15 -0400

    Refactor User entity and add id field

--- server/src/main/java/com/congressconnection/conspring/model/User.java
+++ server/src/main/java/com/congressconnection/conspring/model/User.java
@@ -1,40 +1,17 @@
-package com.example.demo.user;
+package com.congressconnection.conspring.model;
 
-import javax.annotation.processing.Generated;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "user_info_and_faves")
+@Table(name = "users")
+@Getter
+@Setter
 public class User {
-
-    @Id private String email;
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) long id;
+    private String username;
+    @JsonIgnore private String password;
     private boolean admin;
-    private String politician_bill_id;
-
-    public User() { }
-    public User(String email, boolean admin, String politician_bill_id) {
-        this.email = email;
-        this.admin = admin;
-        this.politician_bill_id = politician_bill_id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public boolean isAdmin() {
-        return admin;
-    }
-    public void setAdmin(boolean admin) {
-        this.admin = admin;
-    }
-    public String getPolitician_bill_id() {
-        return politician_bill_id;
-    }
-    public void setPolitician_bill_id(String politician_bill_id) {
-        this.politician_bill_id = politician_bill_id;
-    }
-
 }
