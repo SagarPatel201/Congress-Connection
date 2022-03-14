55f3ae7a09a4e2cae4d424d02eeda4c5973132a8
perpyderp <g4m3rall3n@gmail.com>
2022-03-14 13:54:05 -0400

    Added User (Controller, Service, Repository) files

---
+++ server/src/main/java/com/congressconnection/conspring/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.example.demo.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("/users")
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @RequestMapping("/users")
+    public void createUser(@RequestBody User user) {
+        userService.createUser(user);
+    }
+
+
+}
