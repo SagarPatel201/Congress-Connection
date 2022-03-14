7c23a080b2202f86d6443c01fc9e7ad738764808
SagarPatel201 <sagarpatel201@gmail.com>
2022-03-14 14:37:39 -0400

    Change import and package in UserRepository.java

--- server/src/main/java/com/congressconnection/conspring/repository/UserRepository.java
+++ server/src/main/java/com/congressconnection/conspring/repository/UserRepository.java
@@ -1,9 +1,8 @@
-package com.example.demo.user;
+package com.congressconnection.conspring.repository;
 
+import com.congressconnection.conspring.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, String> {
-
-}
+public interface UserRepository extends JpaRepository<User, String> {}
