b2175cb3fb0cf7a89cfc3be7add03998c75d4bba
SagarPatel201 <sagarpatel201@gmail.com>
2022-03-14 14:47:22 -0400

    Add new finder methods to UserRepository

--- server/src/main/java/com/congressconnection/conspring/repository/UserRepository.java
+++ server/src/main/java/com/congressconnection/conspring/repository/UserRepository.java
@@ -4,5 +4,10 @@ import com.congressconnection.conspring.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepository extends JpaRepository<User, String> {}
+public interface UserRepository extends JpaRepository<User, Long> {
+    public Optional<User> findByUsername(String username);
+    public boolean existsByUsername(String username);
+}
