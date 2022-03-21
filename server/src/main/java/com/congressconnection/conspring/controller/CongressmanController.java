3aa691196942808ad0e184cfe459856329cdc9a9
SagarPatel201 <sagarpatel201@gmail.com>
2022-03-21 17:58:50 -0400

    Add CORS support to CongressmanController

--- server/src/main/java/com/congressconnection/conspring/controller/CongressmanController.java
+++ server/src/main/java/com/congressconnection/conspring/controller/CongressmanController.java
@@ -4,17 +4,16 @@ import com.congressconnection.conspring.service.CongressmanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/")
+@CrossOrigin
+@RequestMapping("/api")
 public class CongressmanController {
     @Autowired
     CongressmanService congressmanService;
