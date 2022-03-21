9d772f63c729ffab5c5a6f57092e8f14567af211
perpyderp <g4m3rall3n@gmail.com>
2022-03-21 19:29:45 -0400

    -Changed password annotation (@JsonIgnore -> JsonProperty)

--- server/src/main/java/com/congressconnection/conspring/model/User.java
+++ server/src/main/java/com/congressconnection/conspring/model/User.java
@@ -1,18 +1,20 @@
 package com.congressconnection.conspring.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 import javax.persistence.*;
 
 @Entity
 @Table(name = "users")
-@Getter
-@Setter
+@Getter @Setter
 public class User {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) long id;
     private String username;
-    @JsonIgnore private String password;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY) private String password;
     private String roles;
     private boolean active;
+
+    public String toString() { return "Username: " + username + "\nRole(s): " + roles + "\nStatus: " + isActive(); }
 }
