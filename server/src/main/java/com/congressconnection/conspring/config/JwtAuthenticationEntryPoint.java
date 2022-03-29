a1861e7e38515669a8c3385730bcf20364c67cd9
perpyderp <g4m3rall3n@gmail.com>
2022-03-29 14:26:23 -0400

    -Created JwtAuthenticationEntryPoint.java to check unauthorized authentications

---
+++ server/src/main/java/com/congressconnection/conspring/config/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,17 @@
+//package com.congressconnection.conspring.config;
+//
+//import org.springframework.security.core.AuthenticationException;
+//import org.springframework.security.web.AuthenticationEntryPoint;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//
+//public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+//
+//    @Override
+//    public void commence(HttpServletRequest request, HttpServletResponse response,
+//                         AuthenticationException authException) throws IOException {
+//        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+//    }
+//}
