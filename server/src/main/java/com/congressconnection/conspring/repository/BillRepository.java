7bf2d0eac2d5ebc7b85dc0129a6e55758259cbd9
Sagar Patel <sagarpatel201@gmail.com>
2022-03-30 20:24:51 -0400

    Add Bill Jpa repository

---
+++ server/src/main/java/com/congressconnection/conspring/repository/BillRepository.java
@@ -0,0 +1,15 @@
+package com.congressconnection.conspring.repository;
+
+import com.congressconnection.conspring.model.Bill;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface BillRepository extends JpaRepository<Bill, Long> {
+    Page<Bill> findByBillType(String billType, Pageable pageRequest);
+    Optional<Bill> findByBillTypeAndBillNumber(String billType, int billNumber);
+}
