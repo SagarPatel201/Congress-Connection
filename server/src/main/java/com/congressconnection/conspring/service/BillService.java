cd8653be8022610e1d62d549e89131e5fc06bd81
Sagar Patel <sagarpatel201@gmail.com>
2022-03-30 20:25:17 -0400

    Add Bill service class

---
+++ server/src/main/java/com/congressconnection/conspring/service/BillService.java
@@ -0,0 +1,33 @@
+package com.congressconnection.conspring.service;
+
+import com.congressconnection.conspring.model.Bill;
+import com.congressconnection.conspring.repository.BillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
+
+@Service
+public class BillService {
+    @Autowired
+    private BillRepository billRepository;
+
+    public Page<Bill> getAllPaginated(int pageNumber, int pageSize) {
+        PageRequest pageRequest = PageRequest.of(pageNumber, pageSize);
+        return billRepository.findAll(pageRequest);
+    }
+
+    public Page<Bill> getByBillType(String billType, int pageNumber, int pageSize) {
+        PageRequest pageRequest = PageRequest.of(pageNumber, pageSize);
+        return billRepository.findByBillType(billType, pageRequest);
+    }
+
+    public Optional<Bill> getByOriginChamberAndBillNumber(String billType, int billNumber) {
+        return billRepository.findByBillTypeAndBillNumber(billType, billNumber);
+    }
+
+}
