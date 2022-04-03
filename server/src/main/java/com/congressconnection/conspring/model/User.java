8d9504c15d3a02b05c3435c86dbde23ec09f9a88
Anthony Hebert <anthony.hebert@rutgers.edu>
2022-04-02 21:47:16 -0400

    Deleted bottom four unused methods

--- server/src/main/java/com/congressconnection/conspring/model/User.java
+++ server/src/main/java/com/congressconnection/conspring/model/User.java
@@ -15,22 +15,4 @@ public class User {
     @JsonIgnore private String password;
     private String roles;
     private boolean active;
-
-    @ManyToMany(cascade = {
-        CascadeType.PERSIST,
-        CascadeType.MERGE
-    })
-    @JoinTable(name = "user_groups",
-            joinColumns =@JoinColumn(name = "customer_id"),
-            inverseJoinColumns = @JoinColumn(name = "group_id"
-    ))
-    private Set<Group> userGroups= new HashSet<>();
-
-    public Set<Group> getUserGroups() {
-        return userGroups;
-    }
-
-    public void setUserGroups(Set<Group> userGroups) {
-        this.userGroups = userGroups;
-    }
 }
