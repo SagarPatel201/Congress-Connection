0bb5036ea626511d254b2782cb033aed40d30291
Sagar Patel <sagarpatel201@gmail.com>
2022-04-04 19:13:17 -0400

    Add CORS header to BillController.

--- server/src/main/java/com/congressconnection/conspring/controller/BillController.java
+++ server/src/main/java/com/congressconnection/conspring/controller/BillController.java
@@ -7,11 +7,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@CrossOrigin
 @RequestMapping("/api/bills")
 public class BillController {
     @Autowired
