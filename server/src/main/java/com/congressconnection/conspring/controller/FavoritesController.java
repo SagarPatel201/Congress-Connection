63267b5920d7c11f211b48903a5a5654a7e2c845
perpyderp <g4m3rall3n@gmail.com>
2022-04-15 22:48:33 -0400

    -Updated code to display the favorite politician/bill information instead of just the IDs

--- server/src/main/java/com/congressconnection/conspring/controller/FavoritesController.java
+++ server/src/main/java/com/congressconnection/conspring/controller/FavoritesController.java
@@ -1,7 +1,11 @@
 package com.congressconnection.conspring.controller;
 
+import com.congressconnection.conspring.model.Bill;
+import com.congressconnection.conspring.model.Congressman;
 import com.congressconnection.conspring.model.FavoriteBills;
 import com.congressconnection.conspring.model.FavoritePolitician;
+import com.congressconnection.conspring.service.BillService;
+import com.congressconnection.conspring.service.CongressmanService;
 import com.congressconnection.conspring.service.FavoriteBillService;
 import com.congressconnection.conspring.service.FavoritePoliticianService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -19,12 +24,31 @@ public class FavoritesController {
 
     @Autowired FavoriteBillService favoriteBillService;
     @Autowired FavoritePoliticianService favoritePoliticiansService;
+    @Autowired CongressmanService politicianService;
+    @Autowired BillService billService;
 
     @GetMapping("/politicians/{id}")
     @PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
     public ResponseEntity<?> getFavoritePoliticians(@PathVariable long id) {
         List<FavoritePolitician> favoritePoliticianList = favoritePoliticiansService.getByUserId(id);
-        return new ResponseEntity<>(favoritePoliticianList, HttpStatus.OK);
+        List<Congressman> politicianList = new ArrayList<>();
+        for(int i = 0; i < favoritePoliticianList.size(); i++) {
+            politicianList.add(politicianService.getById(favoritePoliticianList.get(i).getPoliticianId()));
+        }
+        return new ResponseEntity<>(politicianList, HttpStatus.OK);
+    }
+
+    @GetMapping("/bills/{id}")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
+    public ResponseEntity<?> getFavoriteBills(@PathVariable long id) {
+        List<FavoriteBills> favoriteBillsList = favoriteBillService.getByUserId(id);
+        List<Bill> billList = new ArrayList<>();
+        for(int i = 0; i < favoriteBillsList.size(); i++) {
+            billList.add(billService.getByBillNumber(favoriteBillsList.get(i).getBillNumber(),
+                    favoriteBillsList.get(i).getBillType()));
+        }
+
+        return new ResponseEntity<>(favoriteBillsList, HttpStatus.OK);
     }
 
     @PostMapping("/politician")
@@ -47,12 +71,5 @@ public class FavoritesController {
 
         favoriteBillService.favoriteBill(favoriteBill);
         return new ResponseEntity<>("Favorite successful", HttpStatus.OK);
-    }
-
-    @GetMapping("/bills/{id}")
-    @PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
-    public ResponseEntity<?> getFavoriteBills(@PathVariable long id) {
-        List<FavoriteBills> favoriteBillsList = favoriteBillService.getByUserId(id);
-        return new ResponseEntity<>(favoriteBillsList, HttpStatus.OK);
     }
 }
