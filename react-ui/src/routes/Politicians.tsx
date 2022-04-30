ddfa813a7ca15b9f60915ac6e283b5fadd4db3b6
Sagar Patel <sagarpatel201@gmail.com>
2022-04-30 16:57:07 -0400

    Add new loading and errors to Politicians.tsx

--- react-ui/src/routes/Politicians.tsx
+++ react-ui/src/routes/Politicians.tsx
@@ -1,5 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import PoliticiansTable from '../components/PoliticiansTable';
+import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
+import {CircularProgress} from "@mui/material";
 
 function Politicians() {
     const [error, setError] = useState(null);
@@ -11,7 +13,21 @@ function Politicians() {
           .then(
             (result) => {
               setIsLoaded(true);
-              setItems(result);
+              setItems(result.map( (congressman: { [x: string]: any; }) => {
+                    return {
+                        firstName: congressman['firstName'],
+                        lastName: congressman['lastName'],
+                        chamber: congressman['chamber'],
+                        party: congressman['party'],
+                        state: congressman['state'],
+                        district: congressman['chamber'] === 'senate' ? '' : (congressman['district'] === 0 ? 'At-Large' : congressman['district']),
+                        phone: congressman['phone'],
+                        address: congressman['address'],
+                        reelectionDate: congressman['reelectionDate'],
+                        contactLink: congressman['contactLink'],
+                    }
+                  })
+              );
               console.log(result);
             },
             (error) => {
@@ -23,11 +39,32 @@ function Politicians() {
       
     if (error){
         return(
-            <p>Error Loading JSON</p>
+            <div style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh',
+                width: '100vw',
+                flexDirection: 'column',
+                textAlign: 'center'
+            }}>
+                <ErrorOutlineIcon style={{fontSize: '100px'}}/>
+                <h2>There was an error trying to fetch data from the server. Please try again later.</h2>
+            </div>
         )
     }else if(!isLoaded){
         return(
-            <p>Loading JSON results</p>
+            <div style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh',
+                width: '100vw',
+                flexDirection: 'column',
+                textAlign: 'center'
+            }}>
+                <CircularProgress />
+            </div>
         )
     } else{
         return(
