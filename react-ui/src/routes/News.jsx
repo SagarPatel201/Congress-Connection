6075b0e433875d4dfe3571446c52146e094415fa
Sagar Patel <sagarpatel201@gmail.com>
2022-05-01 17:10:09 -0400

    Fix news page.

--- react-ui/src/routes/News.jsx
+++ react-ui/src/routes/News.jsx
@@ -1,51 +1,25 @@
 import React, {useEffect} from 'react';
-import {Card, CardContent, CardMedia} from '@material-ui/core';
+import {Card, CardContent, CardMedia} from '@mui/material';
+import InfiniteScroll from 'react-infinite-scroller';
 import axios from 'axios';
 import {CircularProgress, Grid, Typography} from "@mui/material";
 import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
-
-function Article({article}) {
-    return (
-        <div>
-            {article && (
-                <Card>
-                    <CardMedia
-                        component={'img'}
-                        src={article.multimedia?.[0]?.url ? `https://nytimes.com/${article.multimedia[0].url}` : 'https://upload.wikimedia.org/wikipedia/commons/4/40/New_York_Times_logo_variation.jpg'}
-                    />
-                    <CardContent>
-                        <Typography variant="h6">
-                            <a href={article.web_url}>
-                                {article.headline.main}
-                            </a>
-                        </Typography>
-                        <Typography variant="subtitle2">
-                            {article.byline.original}
-                        </Typography>
-                        <Typography variant="p">
-                            {article.snippet}
-                        </Typography>
-                    </CardContent>
-                </Card>
-            )}
-        </div>
-    )
-}
 
 function News() {
     const [errored, setErrored] = React.useState(false);
     const [isLoading, setIsLoading] = React.useState(true);
     const [articles, setArticles] = React.useState([]);
+    const [page, setPage] = React.useState(0);
 
     useEffect(() => {
         const getArticles = async () => {
-            const response = await axios.get(`https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=section_name:("us")&sort=newest&api-key=${process.env.REACT_APP_NYTIMES_API_KEY}`);
-            setArticles(response.data.response.docs);
+            const response = await axios.get(`https://api.nytimes.com/svc/search/v2/articlesearch.json?page=${page}&fq=headline:(%22Congress%22)&sort=newest&api-key=${process.env.REACT_APP_NYTIMES_API_KEY}`);
+            setArticles(articles.concat(response.data.response.docs));
             setIsLoading(false);
         };
         getArticles()
             .catch(() => setErrored(true));
-    }, []);
+    }, [page]);
 
     if (errored) {
         return (
@@ -79,21 +53,56 @@ function News() {
     } else {
         return (
             <div>
-                <Grid
-                    container
-                    direction="row"
-                    justify="center"
-                    alignItems="center"
-                >
-                    {articles.map(article => (
-                        <Grid item xs={12} sm={4} key={article._id}>
-                            <Article article={article}/>
+                <InfiniteScroll
+                    pageStart={0}
+                    loadMore={() => setPage(page + 1)}
+                    hasMore={page < 5}
+                    initialLoad={true}
+                    loader={<CircularProgress key={0}/>}
+                    >
+                        <Grid
+                            className={'news-grid'}
+                            container
+                            direction="row"
+                            justify="center"
+                            alignItems="center"
+                            spacing={4}
+                            padding={4}
+                        >
+                            {articles.map(article => (
+                                <Grid className="news-row" item xs={12} sm={2} key={article._id}>
+                                    <Card
+                                        className={"article-card"}
+                                        style={{
+                                            maxWidth: 300,
+                                            maxHeight: 400,
+                                        }}
+                                    >
+                                        <CardMedia
+                                            component={'img'}
+                                            src={article.multimedia?.[0]?.url ? `https://nytimes.com/${article.multimedia[0].url}` : 'https://upload.wikimedia.org/wikipedia/commons/4/40/New_York_Times_logo_variation.jpg'}
+                                        />
+                                        <CardContent>
+                                            <Typography variant="h6">
+                                                <a href={article.web_url}>
+                                                    {article.headline.main}
+                                                </a>
+                                            </Typography>
+                                            <Typography variant="subtitle2">
+                                                {article.byline.original}
+                                            </Typography>
+                                            <Typography variant="p">
+                                                {article.snippet}
+                                            </Typography>
+                                        </CardContent>
+                                    </Card>
+                                </Grid>
+                            ))}
                         </Grid>
-                    ))}
-                </Grid>
-            </div>
-        );
-    }
-}
+                        </InfiniteScroll>
+                    </div>
+                        );
+                    }
+                }
 
-export default News;
+                export default News;
