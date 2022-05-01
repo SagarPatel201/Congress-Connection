5623e594a805321f1000de31e6f5b77b58e0a0d0
Sagar Patel <sagarpatel201@gmail.com>
2022-04-30 23:38:58 -0400

    Add background image to Homepage

--- react-ui/src/routes/Homepage.tsx
+++ react-ui/src/routes/Homepage.tsx
@@ -1,6 +1,18 @@
+import {Paper} from "@mui/material";
+
 export default function Homepage() {
   return (
     <div>
+        <Paper style={{
+            backgroundImage: "url(/capitolhill.jpeg)",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            height: "100vh",
+            width: "100vw",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+        }} />
     </div>
   );
 }
