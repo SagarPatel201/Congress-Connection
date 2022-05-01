366a5094bc5172903865c0d5234735994bbe46c1
perpyderp <g4m3rall3n@gmail.com>
2022-05-01 12:12:26 -0400

    -Cleaned code

--- server/src/main/java/com/congressconnection/conspring/model/User.java
+++ server/src/main/java/com/congressconnection/conspring/model/User.java
@@ -2,13 +2,15 @@ package com.congressconnection.conspring.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import javax.persistence.*;
 
 @Entity
 @Table(name = "users")
-@Getter @Setter
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor
 public class User {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) long id;
     private String username;
